13eaf80deb4e2ac4666e2b14587aad10900d2306
Madushanka Ishara <madushankaishara48@gmail.com>
2023-04-12 20:29:37 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -18,32 +18,172 @@ class Home extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 9, 99, 247),
         elevation: 0,
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to vision mate',
-          style: TextStyle(
-            fontFamily: 'Lato',
-            fontWeight: FontWeight.w600,
-            color: Color.fromARGB(255, 154, 159, 168),
-            fontSize: 20,
-          ),
-        ),
-      ),
+      body: Container(
+          color: const Color.fromARGB(255, 244, 243, 243),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(bottom: 18),
+                padding: const EdgeInsets.fromLTRB(0, 60, 0, 60),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 232, 232, 234),
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  //Name card
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "487,287",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 100, 99, 99),
+                            fontFamily: 'Lato',
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Text(
+                      "Blind",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Lato',
+                          color: Color.fromARGB(255, 148, 148, 148),
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 232, 232, 234),
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  //Name card
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Madushanka Ishara",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 100, 99, 99),
+                            fontFamily: 'Lato',
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Text(
+                      "Member Since 27 Septembger 2020",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Lato',
+                          color: Color.fromARGB(255, 148, 148, 148),
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(255, 9, 99, 247)),
+                      padding: MaterialStatePropertyAll(
+                          EdgeInsets.only(top: 16, bottom: 16)),
+                    ),
+                    child: const Text(
+                      "Learn to answer a call",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 225, 227, 230),
+                          fontFamily: 'Lato',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.6),
+                    )),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 232, 232, 234),
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  //Name card
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      "You will recive a notification when someone \n needs your help",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Lato',
+                          color: Color.fromARGB(255, 148, 148, 148),
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 9, 99, 247),
-        onPressed: () {},
-        child: const Text(
-          '+',
-          style: TextStyle(fontSize: 30),
-        ),
-      ),
+          backgroundColor: const Color.fromARGB(255, 9, 99, 247),
+          onPressed: () {},
+          child: const Icon(
+            Icons.camera,
+            size: 28,
+            color: Color.fromARGB(255, 237, 238, 241),
+          )
+          // child: const Text(
+          //   '+',
+          //   style: TextStyle(fontSize: 30),
+          // ),
+          ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         notchMargin: 5.0,
         shape: const CircularNotchedRectangle(),
         // color: const Color.fromARGB(255, 9, 99, 247),
-        child: Container(
-          height: 70.0,
+        child: SizedBox(
+          height: 75.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.mail,
+                    size: 28,
+                    color: Color.fromARGB(255, 9, 99, 247),
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.chat,
+                    size: 28,
+                    color: Color.fromARGB(255, 9, 99, 247),
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.chat,
+                    size: 28,
+                    color: Color.fromARGB(255, 9, 99, 247),
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.settings,
+                    size: 28,
+                    color: Color.fromARGB(255, 9, 99, 247),
+                  ))
+            ],
+          ),
         ),
       ),
     );
